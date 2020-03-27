e281eaa64608bf60f93d51938b9c4567868c1cd7
v-julia <vjulia94@gmail.com>
2020-03-27 18:06:15 +0300

    fixed changes

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -4,175 +4,57 @@
 namespace Afina {
     namespace Backend {
 
-     // See MapBasedGlobalLockImpl.h
-        bool SimpleLRU::Put(const std::string& key, const std::string& value)
+        SimpleLRU::~SimpleLRU()
         {
-
-            std::size_t sz_new_add = key.size() + value.size();
-
-            if ( sz_new_add > _max_size ) return false; // в любом случае превышает лимит
-
-            // теперь новую запись всегда можно добавить в список, в том числе за счет удаления остальных
-
-            // рассмотрим два варианта: 
-            // вариант 1: добавление новой записи с этим ключом
-            // вариант 2: замена значения, если запись с таким ключом уже есть
-
-            //std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator it;
-            auto it = _lru_index.find(const_cast<std::string&>( key ));
-            //auto it = _lru_index.find(std::ref( key ));
-
-            if ( it == _lru_index.end() ) {
-                // вариант 1: нет записи с key => добавить новый key-value
-                // вначале освободить место, чтобы не превысить размер хранилища
-                while ( sz_new_add > ( _max_size - sz_current ) ) {
-                    std::size_t sz_free = _lru_head->key.size() + _lru_head->value.size();
-                    _lru_index.erase(_lru_head->key);
-                    _lru_head = std::move(_lru_head->next);
-                    _lru_head->prev = nullptr;
-                    sz_current -= sz_free;
+            _lru_index.clear();
+            if ( _lru_head ) {
+                lru_node* lru;
+                while ( ( lru = tail->prev ) != nullptr ) {
+                    lru->next.reset();
+                    tail = lru;
                 }
-                // добавить запись
-                if ( tail == nullptr ) {
-                    _lru_head = std::move(std::unique_ptr<lru_node>(new lru_node)); //std::make_unique<lru_node>());//c++14
-                    _lru_head->key = key;
-                    _lru_head->value = value;
-                    _lru_head->prev = nullptr;
-                    tail = _lru_head.get();
-                }
-                else {
-                    tail->next = std::move(std::unique_ptr<lru_node>(new lru_node));
-                    tail->next->prev = tail;
-                    tail = tail->next.get();
-                    tail->key = key;
-                    tail->value = value;
-                }
-                std::pair<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>> g(tail->key, *tail);
-                _lru_index.emplace(g);
-                sz_current += sz_new_add;
+                _lru_head.reset();
             }
-            else {
-                // вариант 2: записи с key есть => заменить новым value
-                // аналогично - вначале освободить место
-                // вычислить: сколько потребуется свободного места
-                sz_new_add -= it->second.get().key.size();
-                std::unique_ptr<lru_node>* node_for_remove = &_lru_head;
-                while ( sz_new_add > ( _max_size - sz_current ) ) {
-                    if ( node_for_remove->get()->key != key ) {
-                        std::size_t sz_free = node_for_remove->get()->key.size() + node_for_remove->get()->value.size();
-                        _lru_index.erase(node_for_remove->get()->key);
-                        sz_current -= sz_free;
-                        *node_for_remove = std::move(node_for_remove->get()->next);
-                    }
-                    else {
-                        node_for_remove = &( node_for_remove->get()->next );
-                    }
-                }
-                // теперь заменить значение
-                it->second.get().value = value;
-                sz_current += sz_new_add;
+        }
+
+        // расчистить необходимое место в хранилище
+        bool SimpleLRU::free_up_storage_space(std::size_t size_required)
+        {
+            if ( size_required > _max_size ) return false;
+
+            while ( size_required > ( _max_size - sz_current ) ) {
+                remove_node(*_lru_head.get());
             }
 
             return true;
         }
 
-        // See MapBasedGlobalLockImpl.h
-        bool SimpleLRU::PutIfAbsent(const std::string& key, const std::string& value)
+
+        // добавить в хранилище новую запись, при этом заведомо не должно быть там записи с key
+        void SimpleLRU::insert_new_node_to_tail(const std::string& key, const std::string& value)
         {
-
-            // также как и Put, но без замены уже существующей записи
-
-            std::size_t sz_new_add = key.size() + value.size();
-
-            if ( sz_new_add > _max_size ) return false;
-
-
-            std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator it;
-            it = _lru_index.find(const_cast<std::string&>( key ));
-
-            if ( it == _lru_index.end() ) {
-                while ( sz_new_add > ( _max_size - sz_current ) ) {
-                    std::size_t sz_free = _lru_head->key.size() + _lru_head->value.size();
-                    _lru_index.erase(_lru_head->key);
-                    _lru_head = std::move(_lru_head->next);
-                    _lru_head->prev = nullptr;
-                    sz_current -= sz_free;
-                }
-                // добавить запись
-                if ( tail == nullptr ) {
-                    _lru_head = std::move(std::unique_ptr<lru_node>(new lru_node)); //std::make_unique<lru_node>());//c++14
-                    _lru_head->key = key;
-                    _lru_head->value = value;
-                    _lru_head->prev = nullptr;
-                    tail = _lru_head.get();
-                }
-                else {
-                    tail->next = std::move(std::unique_ptr<lru_node>(new lru_node));
-                    tail->next->prev = tail;
-                    tail = tail->next.get();
-                    tail->key = key;
-                    tail->value = value;
-                }
-                std::pair<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>> g(tail->key, *tail);
-                _lru_index.emplace(g);
-                sz_current += sz_new_add;
+            auto new_node = std::unique_ptr<lru_node>(new lru_node(key, value)); //std::make_unique<lru_node>());//c++14
+            if ( !_lru_head ) {
+                _lru_head = std::move(new_node);
+                tail = _lru_head.get();
             }
             else {
-                return false;
+                tail->next = std::move(new_node);
+                tail->next->prev = tail;
+                tail = tail->next.get();
             }
-
-            return true;
+            std::pair<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>> g(tail->key, *tail);
+            _lru_index.emplace(g);
+            sz_current += ( key.size() + value.size() );
         }
 
-        // See MapBasedGlobalLockImpl.h
-        bool SimpleLRU::Set(const std::string& key, const std::string& value)
+
+        // удаление из списка и из индекса (считается, что такой node есть)
+        void SimpleLRU::remove_node(lru_node& node)
         {
-
-           // также как и Put, но только для существующей записи с таким key
-
-            std::size_t sz_new_add = key.size() + value.size();
-
-            if ( sz_new_add > _max_size ) return false; // в любом случае превышает лимит
-
-            std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator it;
-            it = _lru_index.find(const_cast<std::string&>( key ));
-
-            if ( it == _lru_index.end() ) {
-                // нет такой записи с key => false
-                return false;
-            }
-            else {
-                // запись с key есть => заменить новым value
-                // наверное здесь нельзя удалять старые записи, а только изменить заданную
-                // при этом должно остаться место в хранилище
-                sz_new_add -= ( it->second.get().value.size() + key.size() );
-                if ( sz_new_add > ( _max_size - sz_current ) ) return false;
-                it->second.get().value = value;
-                sz_current += sz_new_add;
-                //// поднять рейтинг записи после каждого обращения
-                //if ( it->second.get().next ) {
-                //    it->second.get().next.swap(it->second.get().next->next);
-                //    std::swap(it->second.get().prev, it->second.get().next->prev);
-                //}
-            }
-
-            return true;
-        }
-
-        // See MapBasedGlobalLockImpl.h
-        bool SimpleLRU::Delete(const std::string& key)
-        {
-
-            std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator it;
-            it = _lru_index.find(const_cast<std::string&>( key ));
-
-            if ( it == _lru_index.end() ) return false; // не найден ключ
-
-            // удалить запись с заданным ключом
-            lru_node& node_for_delete = it->second.get();
-            std::size_t sz_free = node_for_delete.key.size() + node_for_delete.value.size();
+            sz_current -= ( node.key.size() + node.value.size() );
             // из индекса
-            _lru_index.erase(it->second.get().key);
+            _lru_index.erase(node.key);
 
             // из хранилища
             if ( _lru_index.size() == 0 ) {
@@ -180,44 +62,103 @@ namespace Afina {
                 tail = nullptr;
                 sz_current = 0;
             }
+            else if ( node.prev == nullptr ) {
+                _lru_head = std::move(_lru_head->next);
+                _lru_head->prev = nullptr;
+            }
             else {
-                if ( node_for_delete.prev == nullptr ) {
-                    _lru_head = std::move(_lru_head->next);
-                    _lru_head->prev = nullptr;
+                lru_node* lru = node.prev;
+                lru->next = std::move(node.next);
+                if ( lru->next ) {
+                    lru->next->prev = lru;
                 }
                 else {
-                    lru_node* lru = node_for_delete.prev;
-                    lru->next=std::move(node_for_delete.next);
-                    if ( lru->next ) {
-                        lru->next->prev = lru;
-                    }
-                    else {
-                        tail = lru;
-                    }
+                    tail = lru;
                 }
-                sz_current -= sz_free;
+            }
+        }
+
+
+        void SimpleLRU::move_node_to_tail(lru_node& node)
+        {
+            if ( &node == tail ) return;
+
+            /** далее можно считать, что есть как минимум два lru_node в списке
+            * при этом выполнены такие условия
+            *  tail != lru_head->get()
+            *  node.next != nullptr, иначе он был бы последним, то есть tail
+            **/
+
+            if ( &node == _lru_head.get() ) {
+                _lru_head.swap(node.next); // node.next - показывает на себя
+                _lru_head->prev = nullptr;
+            }
+            else {
+                node.next->prev = node.prev;
+                node.prev->next.swap(node.next); // точно также node.next - показывает на себя
+            }
+            tail->next.swap(node.next); // теперь за tail следует node
+            node.prev = tail;           // предшествующий к node - tail (фактически node становится последним)
+            tail = &node;               // формально обозначить node как tail
+        }
+
+
+
+
+        bool SimpleLRU::Put(const std::string& key, const std::string& value)
+        {
+            if ( !free_up_storage_space(key.size() + value.size()) ) return false;
+
+            auto it = _lru_index.find(key);
+
+            if ( it == _lru_index.end() ) { // вариант 1: нет записи с key => добавить новый key-value
+                insert_new_node_to_tail(key, value);
+            }
+            else {                          // вариант 2: изменить value в записи с key
+                it->second.get().value = value;
+                sz_current += value.size();
+                sz_current -= it->second.get().value.size(); // на всякий случай, если value.size()<it->second.get().value.size()
             }
             return true;
         }
 
-        // See MapBasedGlobalLockImpl.h
+
+        bool SimpleLRU::PutIfAbsent(const std::string& key, const std::string& value)
+        {
+            if ( _lru_index.find(key) != _lru_index.end() ) return false;
+            return Put(key, value);
+        }
+
+
+        bool SimpleLRU::Set(const std::string& key, const std::string& value)
+        {
+            if ( _lru_index.find(key) == _lru_index.end() ) return false;
+            return Put(key, value);
+        }
+
+
+        bool SimpleLRU::Delete(const std::string& key)
+        {
+            auto it = _lru_index.find(key);
+            if ( it == _lru_index.end() ) return false; // не найден ключ
+            remove_node(it->second.get());
+            return true;
+        }
+
+
         bool SimpleLRU::Get(const std::string& key, std::string& value)
         {
-
-            std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>::iterator it;
-            it = _lru_index.find(const_cast<std::string&>( key ));
-
+            auto it = _lru_index.find(key);
             if ( it == _lru_index.end() ) return false; // не найден ключ
 
             value = it->second.get().value;
-            //// поднять рейтинг записи после каждого обращения
-            //if ( it->second.get().next ) {
-            //    it->second.get().next.swap(it->second.get().next->next);
-            //    std::swap(it->second.get().prev, it->second.get().next->prev);
-            //}
+
+            // поднять рейтинг записи после каждого обращения
+            move_node_to_tail(it->second.get());
 
             return true;
         }
+
 
     } // namespace Backend
 } // namespace Afina
--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -3,81 +3,84 @@
 
 #include <map>
 #include <memory>
-#include <mutex>
 #include <string>
 
 #include <afina/Storage.h>
 
 
-#include <list>
-
 namespace Afina {
-namespace Backend {
+    namespace Backend {
 
-/**
- * # Map based implementation
- * That is NOT thread safe implementaiton!!
- */
-class SimpleLRU : public Afina::Storage {
-public:
-    SimpleLRU(std::size_t max_size = 1024) : _max_size(max_size) {}
+        class SimpleLRU : public Afina::Storage
+        {
+        public:
+            SimpleLRU(std::size_t max_size = 1024) : _max_size(max_size) {}
 
-    ~SimpleLRU() {
-        _lru_index.clear();
-        if( _lru_head ){
-        lru_node* lru;
-        while ( (lru=tail->prev) != nullptr ) {
-            lru->next.reset();
-            tail = lru;
-        }
-        _lru_head.reset(); // TODO: Here is stack overflow
-        }
-    }
+            ~SimpleLRU();
 
-    // Implements Afina::Storage interface
-    bool Put(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool Put(const std::string& key, const std::string& value) override;
 
-    // Implements Afina::Storage interface
-    bool PutIfAbsent(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool PutIfAbsent(const std::string& key, const std::string& value) override;
 
-    // Implements Afina::Storage interface
-    bool Set(const std::string &key, const std::string &value) override;
+            // Implements Afina::Storage interface
+            bool Set(const std::string& key, const std::string& value) override;
 
-    // Implements Afina::Storage interface
-    bool Delete(const std::string &key) override;
+            // Implements Afina::Storage interface
+            bool Delete(const std::string& key) override;
 
-    // Implements Afina::Storage interface
-    bool Get(const std::string &key, std::string &value) override;
+            // Implements Afina::Storage interface
+            bool Get(const std::string& key, std::string& value) override;
 
-private:
-    // LRU cache node
-    using lru_node = struct lru_node {
-        std::string key;
-        std::string value;
-        lru_node* prev;
-        std::unique_ptr<lru_node> next; 
-    };
-    // Здесь такой вопрос:  не понятно, как применить одновременно unique_ptr для двунаправленного списка
+        private:
+            // LRU cache node
+            using lru_node = struct lru_node
+            {
+                std::string key;
+                std::string value;
+                lru_node* prev;
+                std::unique_ptr<lru_node> next;
+                lru_node(const std::string& key_, const std::string& value_) :
+                    key(key_),
+                    value(value_),
+                    prev(nullptr),
+                    next(nullptr)
+                {}
+            };
 
 
-    // Maximum number of bytes could be stored in this cache.
-    // i.e all (keys+values) must be less the _max_size
-    std::size_t _max_size;
-    std::size_t sz_current = 0;
+            // Maximum number of bytes could be stored in this cache.
+            // i.e all (keys+values) must be less the _max_size
+            std::size_t _max_size;
+            std::size_t sz_current = 0;
 
-    // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
-    // element that wasn't used for longest time.
-    //
-    // List owns all nodes
-    std::unique_ptr<lru_node> _lru_head;
-    lru_node* tail=nullptr;
+            // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
+            // element that wasn't used for longest time.
+            //
+            // List owns all nodes
+            std::unique_ptr<lru_node> _lru_head;
+            lru_node* tail = nullptr;
 
 
-    // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
-};
+            // Index of nodes from list above, allows fast random access to elements by lru_node#key
+            // !здесь по отношению к исходному коду изменено c <std::string> на <const std::string>
+            std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
 
-} // namespace Backend
+            // !добавить вспомогательные функции для того, чтобы большие участки кода не дублировались
+
+            bool free_up_storage_space(std::size_t size_required);
+
+            void insert_new_node_to_tail(const std::string& key, const std::string& value);
+
+            void move_node_to_tail(lru_node& node);
+
+            void remove_node(lru_node& node);
+
+
+        };
+
+    } // namespace Backend
 } // namespace Afina
 
 #endif // AFINA_STORAGE_SIMPLE_LRU_H
